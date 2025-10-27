daf3ffe869d52bb5c82f5354ab8238778fd86d1c
jisub-dev <kimjiseob1209@gmail.com>
2025-10-27 23:11:13 +0900

    refactor: 메서드 분리 및 depth 개선

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,27 +8,28 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        // 자동차 이름 입력 받기
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String inputNames = Console.readLine();
-        String[] carNames = inputCarNames(inputNames);
-
-        // 시도 횟수 입력 받기
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        String[] carNames = inputCarNames();
         int tryCount = inputTryCount();
 
-        // RacingGame 인스턴스를 생성하고 게임 시작
         RacingGame racingGame = new RacingGame(createCars(carNames), tryCount);
         racingGame.startRace();
 
-        // 최종 우승자 출력
         System.out.println("최종 우승자 : " + racingGame.findWinners());
     }
 
     /**
      * 자동차 이름을 입력받고 유효성 검사를 수행한 후 배열로 반환한다.
      */
-    public static String[] inputCarNames(String input) {
+    public static String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputNames = Console.readLine();
+        return validateCarNames(inputNames);
+    }
+
+    /**
+     * 입력된 자동차 이름의 유효성 검사를 수행한다.
+     */
+    public static String[] validateCarNames(String input) {
         String[] carNames = input.split(",");
         if (carNames.length < 1) {
             throw new IllegalArgumentException("자동차 이름을 한 개 이상 입력해야 합니다.");
@@ -49,7 +50,15 @@ public class Application {
      * 시도할 횟수를 입력받고 유효성 검사를 수행한 후 반환한다.
      */
     public static int inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
+        return validateTryCount(input);
+    }
+
+    /**
+     * 입력된 시도 횟수의 유효성 검사를 수행한다.
+     */
+    public static int validateTryCount(String input) {
         try {
             int tryCount = Integer.parseInt(input);
             if (tryCount <= 0) {
